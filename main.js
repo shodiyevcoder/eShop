62f80ae43dd1d5eb19d2a0ac35565d3f558cce43
ShodiyevOybek <oybekshodiyev01@gmail.com>
2022-09-14 18:17:12 +0500

    eshop

---
+++ main.js
@@ -0,0 +1,19 @@
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
